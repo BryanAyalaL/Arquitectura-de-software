5b47865eec47242526db2b02de90a939b9014cd8
Esteban Chavaria <juanchabarria017@gmail.com>
2026-03-26 11:36:46 -0500

    Implement UserRepository class for user management

--- Arquitectura-software-equipo/ src/ services/UserRepository.js
+++ Arquitectura-software-equipo/ src/ services/UserRepository.js
@@ -1,1 +1,35 @@
+class UserRepository {
+  constructor(db) {
+    this.db = db;
+  }
 
+  async getAllUsers() {
+    return await this.db.query("SELECT * FROM users");
+  }
+
+  async getUserById(id) {
+    return await this.db.query("SELECT * FROM users WHERE id = ?", [id]);
+  }
+
+  async createUser(user) {
+    const { name, email } = user;
+    return await this.db.query(
+      "INSERT INTO users (name, email) VALUES (?, ?)",
+      [name, email]
+    );
+  }
+
+  async updateUser(id, user) {
+    const { name, email } = user;
+    return await this.db.query(
+      "UPDATE users SET name = ?, email = ? WHERE id = ?",
+      [name, email, id]
+    );
+  }
+
+  async deleteUser(id) {
+    return await this.db.query("DELETE FROM users WHERE id = ?", [id]);
+  }
+}
+
+module.exports = UserRepository;
