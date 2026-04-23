f5f5f3ae4e31c116f45ae8af8e357fdc7a56499f
Esteban Chavaria <juanchabarria017@gmail.com>
2026-04-23 12:44:34 -0500

    Update server.js

--- Arquitectura-software-equipo/ src/server.js
+++ Arquitectura-software-equipo/ src/server.js
@@ -1,1 +1,16 @@
+//IMPORTAR EXPRESS
+const express = require("express");
+//CREAR LA APP
+const app = express();
+//HABILITAR JSON
+app.use(express.json());
+//RUTA DE PRUEBA
+app.get("/", (req, res) => {
+  res.send("API funcionando 🚀");
+});
+//LEVANTAR EL SERVIDOR
+const PORT = 3000;
 
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
