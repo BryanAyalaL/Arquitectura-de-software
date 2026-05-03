c9c4144fea7ac0d1c9ee2cf68ce156da8a31e025
Esteban Chavaria <juanchabarria017@gmail.com>
2026-05-03 14:52:42 -0500

    Add JWT authentication middleware
    Implement JWT authentication middleware to verify tokens.

--- Arquitectura-software-equipo/ src/middleware/authMiddleware.js
+++ Arquitectura-software-equipo/ src/middleware/authMiddleware.js
@@ -1,1 +1,27 @@
+const jwt = require("jsonwebtoken");
 
+const SECRET = "secreto_super_seguro";
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+
+  // ❌ No hay token
+  if (!authHeader) {
+    return res.status(401).json({
+      message: "Acceso denegado: token requerido",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, SECRET);
+
+    req.user = decoded; // guardas el usuario
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Token inválido",
+    });
+  }
+};
