8e57bca9a366807cf09277c63c817d07b67ad2a0
BryanAyalaL <bryyandaniiel@gmail.com>
2026-06-05 17:33:42 -0500

    fic: actualizar la configuración del cliente Redis para usar variables de entorno para el host y el puerto

--- Arquitectura-software-equipo/src/Config/RedisClient.js
+++ Arquitectura-software-equipo/src/Config/RedisClient.js
@@ -1,14 +1,21 @@
 const redis = require("redis");
+const redisHost = process.env.REDIS_HOST || "localhost";
+const redisPort = process.env.REDIS_PORT || 6379;
 
 const client = redis.createClient({
+    // el principio de The Twelve-Factor 
+    url: `redis://${redisHost}:${redisPort}`
 
-    url: "redis://localhost:6379"
+});
 
+client.on("connect", () => {
+
+    console.log(`Conectado exitosamente a Redis en ${redisHost}:${redisPort}`);
 });
 
 client.on("error", (err) => {
 
-    console.error("Redis Error:", err);
+    console.error("Redis Error Critico:", err);
 
 });
 
